ec6b9415e455ca7cf97952082c9d110cef47e01e
Jaime-vargas <jaime.vargas@live.com.mx>
2026-05-04 09:42:08 -0600

    created hero secction

--- src/app/components/hero-component/hero-component.ts
+++ src/app/components/hero-component/hero-component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {NzColDirective, NzRowDirective} from 'ng-zorro-antd/grid';
+import {NzFlexDirective} from 'ng-zorro-antd/flex';
+import {ButtonComponent} from '../button-component/button-component';
+import {NzIconDirective} from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-hero-component',
-  imports: [],
+  imports: [
+    NzRowDirective,
+    NzColDirective,
+    NzFlexDirective,
+    ButtonComponent,
+    NzIconDirective
+  ],
   templateUrl: './hero-component.html',
   styleUrl: './hero-component.css',
 })
