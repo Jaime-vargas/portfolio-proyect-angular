d62dfd33ff4f2636cb2571741c03b35a45c85a10
Jaime-vargas <jaime.vargas@live.com.mx>
2026-05-13 15:40:21 -0600

    added a section player

--- src/app/sections/waveform-section/waveform-section.ts
+++ src/app/sections/waveform-section/waveform-section.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {NzFlexDirective} from 'ng-zorro-antd/flex';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-waveform-section',
-  imports: [],
+  imports: [
+    NzFlexDirective,
+    NgOptimizedImage
+  ],
   templateUrl: './waveform-section.html',
   styleUrl: './waveform-section.css',
 })
 export class WaveformSection {
-
+  paragraphs = "Hago que tu marca se entienda... y se escuche."
 }
